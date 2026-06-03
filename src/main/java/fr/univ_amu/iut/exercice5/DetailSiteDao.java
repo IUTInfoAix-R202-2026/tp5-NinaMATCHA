ee303008197064a4b5783c11726c4adfad0e6884
Nina MATIC CHARBIT <nina.MATIC-CHARBIT@etu.univ-amu.fr>
2026-06-03 13:00:29 +0000

    Exercice 5 : tout les test passent

--- src/main/java/fr/univ_amu/iut/exercice5/DetailSiteDao.java
+++ src/main/java/fr/univ_amu/iut/exercice5/DetailSiteDao.java
@@ -57,7 +57,7 @@ public class DetailSiteDao {
   public List<String> findEspecesObserveesSurLeSite(String numeroCarre) {
     List<String> especes = new ArrayList<>();
 
-    // TODO exercice 5 : écrire la jointure entre observation, passage et taxon.
+    // exercice 5 : écrire la jointure entre observation, passage et taxon.
     //
     // Objectif : pour un site donné, lister les noms vernaculaires des espèces détectées,
     // sans doublon, triés.
@@ -71,6 +71,19 @@ public class DetailSiteDao {
     //
     // Préparer la requête, positionner le paramètre, parcourir le ResultSet et ajouter chaque
     // nom à `especes`. Envelopper toute SQLException dans une DataAccessException.
+    String sql =
+        "SELECT DISTINCT t.nom_vernaculaire FROM observation o JOIN passage p ON o.passage_id = p.id JOIN taxon t ON o.code_taxon = t.code WHERE p.numero_carre = ? ORDER BY t.nom_vernaculaire";
+    try (Connection connexion = source.getConnection();
+        PreparedStatement ps = connexion.prepareStatement(sql)) {
+      ps.setString(1, numeroCarre);
+      try (ResultSet rs = ps.executeQuery()) {
+        while (rs.next()) {
+          especes.add(rs.getString("nom_vernaculaire"));
+        }
+      }
+    } catch (SQLException e) {
+      throw new DataAccessException("DataAccessException", e);
+    }
 
     return especes;
   }
